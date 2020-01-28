11e2aa022a09bcbbfa48ce5c9c13bafbd73a4489
Guillermo <alvarezgui546@gmail.com>
2020-01-28 13:33:35 -0500

    first commit

---
+++ detailviews/src/androidTest/java/com/gorillamo/ui/detailview/DetailViewTest.kt
@@ -0,0 +1,24 @@
+package com.gorillamo.ui.detailview
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class DetailViewTest {
+
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.gorillamo.ui.detailview.test", appContext.packageName)
+    }
+}
